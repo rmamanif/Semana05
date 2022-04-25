772bfde8a11089f3abea18e500eb09912d03e525
Richard Mamani <richard.mamani@tecsup.edu.pe>
2022-04-25 00:53:59 -0500

    Agregar archivos de proyecto.

---
+++ ManCategoria.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Business;
+using Entity;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ManCategoria : Window
+    {
+        public int ID { get; set; }
+        public ManCategoria(int Id)
+        {
+            InitializeComponent();
+            ID = Id;
+            if (ID > 0)
+            {
+                BCategoria bCategoria = new BCategoria();
+                List<Categoria> categorias = new List<Categoria>();
+                categorias = bCategoria.Listar(ID);
+                if (categorias.Count > 0)
+                {
+                    txtId.Text = categorias[0].IdCategoria.ToString();
+                    txtNombre.Text = categorias[0].NombreCategoria;
+                    txtDescripcion.Text = categorias[0].Descripcion;
+                }
+            }
+        }
+
+        private void BtnGrabar_Click(object sender, RoutedEventArgs e)
+        {
+            BCategoria bCategoria = null;
+            bool result = true;
+            try
+            {
+                bCategoria = new BCategoria();
+                if (ID > 0)
+                {
+                    result = bCategoria.Actualizar(new Categoria { IdCategoria = ID, NombreCategoria = txtNombre.Text, Descripcion = txtDescripcion.Text });
+                }
+                else
+                    result = bCategoria.Insertar(new Categoria { NombreCategoria = txtNombre.Text, Descripcion = txtDescripcion.Text });
+                if (!result)
+                {
+                    MessageBox.Show("Comunicarse con el administrador  !");
+                }
+                Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Comunicarse con el administrador");
+            }
+            finally
+            {
+                bCategoria = null;
+            }
+        }
+
+        private void BtnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            BCategoria bCategoria = null;
+            bool result = true;
+            try
+            {
+                bCategoria = new BCategoria();
+                if (ID > 0)
+                {
+                    result = bCategoria.Eliminar(ID);
+                }
+                if (result)
+                {
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show(" error para eliminar");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("error");
+            }
+        }
+    }
+}
